e3346afcb34d41e9230e75ba6adf184dbc497055
harakere <83594022+harakere@users.noreply.github.com>
2021-06-18 13:13:35 +0400

    Delete java-data-handling-template-master directory

--- java-data-handling-template-master/src/main/java/com/epam/izh/rd/online/service/RegExpService.java
+++
@@ -1,8 +0,0 @@
-package com.epam.izh.rd.online.service;
-
-public interface RegExpService {
-
-    String maskSensitiveData();
-
-    String replacePlaceholders(double paymentAmount, double balance);
-}
